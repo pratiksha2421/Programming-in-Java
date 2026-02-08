5d1e962468369b67c1ea678a01efe2615d6296be
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-02-08 11:19:43 +0530

    Print final value Java program

---
+++ finalVariable.java
@@ -0,0 +1,6 @@
+public class finalVariable {
+    public static void main(String[] args) {
+        final int myNum = 20;  /* ab yha jo final likha h iska mtlb h ki or koi value agar assign krege ab to vo chnage ni hoga output m kuki 20 ke sath final lga h vhi value rhegi agar final ni hota to fir dusri value assign krne pr value change ho jati h */
+        System.out.println(myNum);
+    }
+}
