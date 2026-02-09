ea0c2f4e1b9b55847d3699f1eece3de9324d9614
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-02-09 08:00:28 +0530

    Print sum of numbers Java program

---
+++ sum.java
@@ -0,0 +1,8 @@
+public class sum {
+    public static void main(String[] args) {
+        int x=4;
+        int y=8;
+        System.out.println("The sum is " + x+y); //bracket ni lgayge to bs + kr dega mtlb aage ki chezzo ko jod dega
+        System.out.println("The sum is " +(x+y));
+    }
+}
