b4d8b6c96d6dbc675f13d2a614fc88bb61509b4c
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-02-10 07:24:40 +0530

    Add two strings in one Java program

---
+++ concatenate.java
@@ -0,0 +1,10 @@
+public class concatenate {
+    public static void main(String[] args) {
+        {
+            String firstName="Pratiksha";
+            String lastName="Mishra";
+            String fullName=firstName +lastName;
+            System.out.println(fullName);
+        }
+    }
+}
