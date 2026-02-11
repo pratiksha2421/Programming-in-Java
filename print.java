152bc2ec6eb5c480b6217320c714e5dca8136422
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-02-11 20:56:24 +0530

    Print name and age using + Java program

---
+++ print.java
@@ -0,0 +1,10 @@
+public class print {
+    public static void main(String[] args)
+
+{
+String name = "Pratiksha";
+int age = 20;
+System.out.println("Name : "+name );
+System.out.println("Age : "+age);
+}
+}
