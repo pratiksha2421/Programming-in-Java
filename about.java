fd4983af75efd2e8e3c1fe290260f7584f12051e
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-02-12 19:02:00 +0530

    Abouit java

---
+++ about.java
@@ -0,0 +1,8 @@
+public class about {
+    public static void main(String[] args) {
+        {
+            System.out.println("My name is Pratiksha Mishra");
+            System.out.println("Age: 20");
+        }
+    }
+}
