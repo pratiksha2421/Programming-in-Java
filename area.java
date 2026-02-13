faee41201e03c24a07ed9aab5e4e8b38f7f8d364
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-02-13 09:31:53 +0530

    Find area java program

---
+++ area.java
@@ -0,0 +1,7 @@
+public class area {
+    public static void main(String[] args)
+    {
+        int length = 5, breadth = 7;
+        System.out.println("Area of rectangle = " + length*breadth);
+    }
+}
