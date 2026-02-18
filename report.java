7e17cadec7e20ebc27aa822c248d6252dcb2c789
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-02-18 08:21:29 +0530

    Student's Report card java program

---
+++ report.java
@@ -0,0 +1,11 @@
+public class report {
+    public static void main(String[] args) {
+        int rollno=122;
+        double score=98.9;
+        char grade='A';
+        System.out.println("Rollno :"+rollno);
+        System.out.println("Score :"+score);
+         System.out.println("Grade :"+grade);
+
+    }
+}
