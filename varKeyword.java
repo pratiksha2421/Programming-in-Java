ba93a32596ef6d400c9f54df7182b87957c67faf
pratiksha2421 <pratikshamishra2421@gmail.com>
2026-02-19 08:22:53 +0530

    Var keyword Java program

---
+++ varKeyword.java
@@ -0,0 +1,10 @@
+public class varKeyword {
+    public static void main(String[] args) {
+        var myNum=5;
+        var charValue='A';
+        var stringName="Rimjhim";
+        System.out.println("Integer value:" + myNum);
+        System.out.println("Character value:" + charValue);
+        System.out.println("String value:" + stringName);
+    }
+}
